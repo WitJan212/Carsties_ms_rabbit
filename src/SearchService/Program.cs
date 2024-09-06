f0caa0c1442b70270ac3efd3ba980911e2fad37c
vit michalec <vit.michalec@construsoft.com>
2024-09-07 00:00:52 +0200

    Added simple resiliency between the services using synchronous HttpClient using the Polly.

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,13 +1,15 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Models;
 using SearchService.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -15,13 +17,35 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+// Call InitializeDb when the application starts
+app.Lifetime.ApplicationStarted.Register(async ()=>
 {
-    await DbInitializer.InitializeDb(app);
-}
-catch (System.Exception exception)
-{
-    Console.WriteLine(exception);
-    throw;
-}
+    try
+    {
+        await DbInitializer.InitializeDb(app);
+    }
+    catch (System.Exception exception)
+    {
+        Console.WriteLine(exception);
+        throw;
+    }
+}); 
+
 app.Run();
+
+
+/// <summary>
+/// Provides a default retry policy for HTTP requests.
+/// </summary>
+/// <remarks>
+/// The policy is configured to retry indefinitely with a 3 second delay between retries.
+/// It will retry on transient HTTP errors and on 404 responses.
+/// </remarks>
+/// <returns>The HTTP request policy.</returns>
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(message => message.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+
+
